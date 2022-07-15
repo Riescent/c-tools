6b6448c952da1eaa175c1b9d75e43cdad663f2de
Riescent <91223304+Riescent@users.noreply.github.com>
2022-07-15 21:37:40 +0200

    fixed 2 bugs in printf_center() and added get_int() in stdio-riescent.c
    int get_int(const char *print):
    prints inputed string
    returns scanf(%i) after making sure user input is an integer
    
    PS: added 'test.c' to '.gitignore'

--- stdio-riescent.c
+++ stdio-riescent.c
@@ -1,11 +1,8 @@
-#include <stdio.h>		//print_center() - printf_center()
-#include <string.h>		//print_center() - printf_center()
-#include <sys/ioctl.h>	//print_center() - printf_center()
-#include <unistd.h>		//print_center() - printf_center()
-#include <stdarg.h>		//printf_center()
-
-
-
+#include <stdio.h>	   //print_center() - printf_center()
+#include <string.h>	   //print_center() - printf_center()
+#include <sys/ioctl.h> //print_center() - printf_center()
+#include <unistd.h>	   //print_center() - printf_center()
+#include <stdarg.h>	   //printf_center()
 
 /*
 #include <stdio.h>		printf()
@@ -14,14 +11,15 @@
 #include <unistd.h>		STDOUT_FILENO
 __TO_DO__ enable support for multiple lines
 */
-void print_center(char str[]) {
+void print_center(const char *str)
+{
 	//= move cursor to center
-		struct winsize window_size;
-		ioctl(STDOUT_FILENO, TIOCGWINSZ, &window_size); // Assigns the size of the terminal to window_size
+	struct winsize window_size;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &window_size); // Assigns the size of the terminal to window_size
 
-		for (int i = 0; i < window_size.ws_col / 2 - (strlen(str) / 2); i++)
-			printf(" ");
-	//! move cursor to center
+	for (int i = 0; i < window_size.ws_col / 2 - (strlen(str) / 2); i++)
+		printf(" ");
+	//!
 
 	printf("%s", str);
 }
@@ -36,32 +34,54 @@ __TO_DO__ find a way to count args to avoid segfault when more args then implied
 __TO_DO__ enable support for multiple lines
 __!WARNING!__ Causes segfault when called with more arguments then implied by format
 */
-void printf_center(char const* format, ...)
+void printf_center(const char *format, ...)
 {
 	//= format string
-		va_list args;
-		char str[1000];
+	va_list args;
+	char print_me[1000];
 
-		va_start(args, format);
-		vsnprintf(str, sizeof(str), format, args); //stores output string to str
-		va_end(args);
-	//! format string
+	va_start(args, format);
+	vsnprintf(print_me, sizeof(print_me) / sizeof(print_me[0]), format, args); // stores output string to str
+	va_end(args);
+	//!
 
 	//= move cursor to center
-		struct winsize window_size;
-		ioctl(STDOUT_FILENO, TIOCGWINSZ, &window_size); // Assigns the size of the terminal to window_size
+	struct winsize window_size;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &window_size); // Assigns the size of the terminal to window_size
 
-		for (int i = 0; i < window_size.ws_col / 2 - (strlen(str) / 2); i++)
-			printf(" ");
-	//! move cursor at to center
+	/* window_size.ws_col / 2 - strlen(print_me) / 2 needs to be stored in a variable
+	   or the following loop bugs when string longer then the terminal can write in one line */
+	int spaces_to_write = window_size.ws_col / 2 - strlen(print_me) / 2;
+	for (int i = 0; i < spaces_to_write; i++)
+		printf(" ");
+	//!
 
-	printf("%s", str);
+	printf("%s", print_me);
 }
 
+//#include <stdio.h>
+int get_int(const char *print)
+{
+	printf("%s", print);
 
-int main(void) {
-	//char str[1000];
-	//snprintf(str, sizeof(str), "test %i\n", 4);
+	int return_integer;
+	if (!scanf("%i", &return_integer))
+	{
+		printf("An error occured, make sure you input an integer. ");
+
+		//= This discards the input from scanf to avoid entering an infinite loop
+		while (return_integer != '\n' && return_integer != EOF)
+			return_integer = getchar();
+		//!
+
+		return_integer = get_int(print); // Restarts the fonction
+	}
+	return (return_integer);
+}
+
+int main(void)
+{
 	print_center("centered text\n");
-	printf_center("str %i %f %c %s\n", 2000000000, 7.8, 'c', "yo");
+	int i = get_int("enter i: ");
+	printf_center("i = %i\n", i);
 }
