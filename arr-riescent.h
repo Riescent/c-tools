4483a2fcd4d531791617c13d008361f3d9acda15
Riescent <91223304+Riescent@users.noreply.github.com>
2022-07-18 23:01:28 +0200

    Update arr-riescent.h

--- arr-riescent.h
+++ arr-riescent.h
@@ -3,11 +3,11 @@
 #include "random-riescent.h"
 
 // Used to get the size of an array
-#define ARRSIZE(arr) sizeof(arr) / sizeof(arr[0])
+#define arrsize(arr) sizeof(arr) / sizeof(arr[0])
 
 //#include "random-riescent.h"
 // Randomize all member of an int array in a set range (min and max are included)
-void randomize_array_int(int *arr, const int arr_size, const int min, const int max)
+void randomize_arr_int(int *arr, const int arr_size, const int min, const int max)
 {
 	for (int i = 0; i < arr_size; i++)
 		arr[i] = random_range(min, max);
@@ -15,7 +15,7 @@ void randomize_array_int(int *arr, const int arr_size, const int min, const int 
 
 //#include <stdbool.h>
 // Sorts an int array from lower to bigger
-void sort_array_int(int *ptr_arr, const int arr_size)
+void sort_arr_int(int *ptr_arr, const int arr_size)
 {
 	bool changed_something = false;
 	int temp;
@@ -38,7 +38,7 @@ void sort_array_int(int *ptr_arr, const int arr_size)
 }
 
 // Does a binary search for 'target' on an int array
-int binary_search_int(const int target, const int *arr, const int arr_size)
+int bi_search_arr_int(const int target, const int *arr, const int arr_size)
 {
 	int mid;
 	int high = arr_size - 1;
