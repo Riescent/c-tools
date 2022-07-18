6e768d359f14ebbd8976be3f8bf956eeb6e12b15
Riescent <91223304+Riescent@users.noreply.github.com>
2022-07-18 19:52:49 +0200

    Update random-riescent.h

--- random-riescent.h
+++ random-riescent.h
@@ -5,9 +5,10 @@
 #include <time.h>
 #include <stdlib.h>
 */
-// Returns random value between min and max (min and max are possible returns)
+// srand(time_ms()); needs to be included in main. random_range() returns random value between min and max (min and max are possible returns).
 int random_range(int min, int max)
 {
-	srand(time_ms()); // Seeds rand() with time_ms() (changes by 1 every ms)
+	// The following needs to be included in main()
+	// srand(time_ms()); // Seeds rand() with time_ms() (changes by 1 every ms)
 	return (rand() % (max + 1 - min) + min);
 }
