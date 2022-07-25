a5c00f981666b9fbf542d5da5885479648d99f58
Riescent <91223304+Riescent@users.noreply.github.com>
2022-07-25 22:42:17 +0200

    Update random-c-tools.h

--- c-tools_headers/random-c-tools.h
+++ c-tools_headers/random-c-tools.h
@@ -2,10 +2,7 @@
 
 #include <stdlib.h>
 
-/*
-#include <time.h>
-#include <stdlib.h>
-*/
+//#include <stdlib.h>
 // srand((int)time_ms()); included in main gives better results. random_range() returns random value between min and max (min and max are possible returns).
 int random_range(const int min, const int max)
 {
